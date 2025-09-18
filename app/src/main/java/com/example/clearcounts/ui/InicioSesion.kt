52f2821de3e5daf061a98ed0f1f37d2f1f46b4d6
MateoGutierrezL <mateogutierrez7112@gmail.com>
2025-09-18 16:31:58 -0500

    Pequeños cambios

--- app/src/main/java/com/example/clearcounts/ui/InicioSesion.kt
+++ app/src/main/java/com/example/clearcounts/ui/InicioSesion.kt
@@ -1,6 +1,5 @@
 package com.example.clearcounts.ui
 
-import android.R
 import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
