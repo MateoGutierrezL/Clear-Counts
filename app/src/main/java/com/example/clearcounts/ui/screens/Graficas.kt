276dcb15e1eb1e4e744786799dbea68a013ca554
Danny <daniellopezafanador3@gmail.com>
2025-11-27 21:06:07 -0500

    Pantalla Graficas, Gastos añadida

--- app/src/main/java/com/example/clearcounts/ui/screens/Graficas.kt
+++ app/src/main/java/com/example/clearcounts/ui/screens/Graficas.kt
@@ -3,13 +3,17 @@ package com.example.clearcounts.ui.screens
 import androidx.compose.animation.core.animateFloatAsState
 import androidx.compose.animation.core.tween
 import androidx.compose.foundation.Canvas
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.isSystemInDarkTheme
 import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.lazy.LazyRow
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Button
 import androidx.compose.material3.ButtonDefaults
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.*
@@ -17,10 +21,15 @@ import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
+import androidx.compose.ui.draw.rotate
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.PaintingStyle.Companion.Stroke
+import androidx.compose.ui.graphics.StrokeCap
+import androidx.compose.ui.graphics.drawscope.Stroke
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.SpanStyle
 import androidx.compose.ui.text.buildAnnotatedString
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.text.withStyle
 import androidx.compose.ui.tooling.preview.Preview
@@ -31,6 +40,7 @@ import androidx.compose.ui.unit.sp
 import com.example.clearcounts.R
 import com.example.clearcounts.ui.theme.AzulEncabezado
 
+@Preview
 @Composable
 fun Graficas() {
     Box(modifier = Modifier.fillMaxSize()) {
@@ -41,7 +51,7 @@ fun Graficas() {
             verticalArrangement = Arrangement.Center,
             horizontalAlignment = Alignment.CenterHorizontally
         ) {
-            Text(text = "Graficas", fontSize = 30.sp, color = AzulEncabezado)
+            Text(text = "Gráficos", fontSize = 30.sp, color = AzulEncabezado)
             Spacer(modifier = Modifier.size(20.dp))
             contenido()
         }
@@ -65,8 +75,10 @@ fun contenido() {
     } else {
         if (gasto) Color.Black else Color.Black
     }
+    val colorTexto = if (isSystemInDarkTheme()) Color.White else Color.Black
 
     Column(
+
         modifier = Modifier
             .padding(start = 20.dp, end = 20.dp, top = 30.dp)
             .clip(RoundedCornerShape(10.dp))
@@ -76,7 +88,9 @@ fun contenido() {
             modifier = Modifier
                 .background(MaterialTheme.colorScheme.secondaryContainer)
         ) {
+
             Column(
+                horizontalAlignment = Alignment.CenterHorizontally,
                 modifier = Modifier
                     .padding(start = 20.dp, end = 20.dp, top = 10.dp)
                     .clip(RoundedCornerShape(10.dp))
@@ -111,12 +125,16 @@ fun contenido() {
                                 .size(width = 180.dp, height = 64.dp)
                                 .padding(top = 15.dp, bottom = 15.dp, end = 7.dp),
                             colors = ButtonDefaults.buttonColors(
-                                containerColor = Color.Blue
+                                containerColor = if (ingreso)
+                                    MaterialTheme.colorScheme.primaryContainer
+                                else
+                                    MaterialTheme.colorScheme.inversePrimary
                             )
                         ) {
                             Text(
                                 text = "Ingreso por categoria",
-                                fontSize = 13.sp
+                                fontSize = 11.sp,
+                                color = colorTextoIngreso
                             )
                         }
                     }
@@ -128,43 +146,85 @@ fun contenido() {
                                 .size(width = 180.dp, height = 64.dp)
                                 .padding(top = 15.dp, bottom = 15.dp),
                             colors = ButtonDefaults.buttonColors(
-                                containerColor = Color.Blue
+                                containerColor = if (ingreso)
+                                    MaterialTheme.colorScheme.primaryContainer
+                                else
+                                    MaterialTheme.colorScheme.inversePrimary
                             )
                         ) {
                             Text(
                                 text = "Balance Mensual",
-                                fontSize = 13.sp
+                                fontSize = 13.sp,
+                                color = colorTextoIngreso
                             )
                         }
                     }
                 }
 
                 Row {
+                    Spacer(modifier = Modifier.width(16.dp))
+                }
+
+                Row(
+                    horizontalArrangement = Arrangement.Center,
+                    modifier = Modifier.fillMaxWidth().padding(top = 10.dp)
+                ) {
+                    IconButton(onClick = {}) {
+                        Icon(
+                            painter = painterResource(id = R.drawable.flecha),
+                            contentDescription = "Flecha Izquierda",
+                            modifier = Modifier
+                                .size(15.dp)
+                                .rotate(180f), tint = colorTexto,
+                        )
+                    }
                     Text(
-                        text = "10/09/2005",
-                        textAlign = TextAlign.Start,
-                        modifier = Modifier.padding(10.dp),
-                        color = MaterialTheme.colorScheme.onSecondaryContainer
+                        text = "Noviembre",
+                        fontSize = 20.sp,
+                        modifier = Modifier.padding(top = 10.dp, end = 35.dp, start = 50.dp), color = colorTexto
                     )
 
-                    Spacer(modifier = Modifier.width(16.dp))
+                    IconButton(onClick = {}) {
+                        Icon(
+                            painter = painterResource(id = R.drawable.flecha),
+                            contentDescription = "Flecha Derecha",
+                            modifier = Modifier
+                                .size(15.dp), tint = colorTexto
+                        )
+                    }
+                }
+                CircularProgressBar(percentage = 0.8f, number = 100)
 
-                    Text(
+                Row(horizontalArrangement = Arrangement.Start, modifier = Modifier.fillMaxWidth()) {
+                    Image(
+                        painter = painterResource(R.drawable.camion),
                         modifier = Modifier
-                            .padding(top = 10.dp)
-                            .fillMaxWidth(),
-                        textAlign = TextAlign.End,
-                        text = buildAnnotatedString {
-                            withStyle(style = SpanStyle(Color.Red)) {
-                                append("$340.000")
-                            }
-                            withStyle(style = SpanStyle(MaterialTheme.colorScheme.onSecondaryContainer)) {
-                                append("/")
-                            }
-                            withStyle(style = SpanStyle(Color.Green)) {
-                                append("$500.000")
-                            }
-                        }
+                            .padding(top = 30.dp, start = 20.dp, end = 1.dp)
+                            .size(30.dp),
+                        contentDescription = "Icono de Transporte"
+                    )
+
+                    Text( text = "Transporte",
+                        fontSize = 10.sp,
+                        fontWeight = FontWeight.Bold,
+                        modifier = Modifier.padding(top = 35.dp, start = 10.dp),
+                    )
+                }
+
+                Text(text = "_________________________________")
+                Row(horizontalArrangement = Arrangement.Start, modifier = Modifier.fillMaxWidth()) {
+                    Image(
+                        painter = painterResource(R.drawable.corazon),
+                        modifier = Modifier
+                            .padding(top = 50.dp, start = 20.dp)
+                            .size(30.dp),
+                        contentDescription = "Icono de Corazon"
+                    )
+
+                    Text( text = "Salud",
+                        fontSize = 10.sp,
+                        fontWeight = FontWeight.Bold,
+                        modifier = Modifier.padding(top = 49.dp, start = 10.dp),
                     )
                 }
             }
@@ -178,7 +238,7 @@ fun CircularProgressBar(
     number: Int,
     fontSize: TextUnit = 28.sp,
     radius: Dp = 50.dp,
-    color: Color = Color.Green,
+    color: Color = AzulEncabezado,
     strokeWidth: Dp = 8.dp,
     animDuration: Int = 1000,
     animDelay: Int = 0
@@ -202,7 +262,19 @@ fun CircularProgressBar(
         modifier = Modifier.size(radius * 2f)
     ) {
         Canvas(modifier = Modifier.size(radius * 2f)) {
-
+         drawArc(
+             color = color,
+             -90f,
+             360 * curPercentage.value,
+             useCenter = false,
+             style = Stroke(strokeWidth.toPx(), cap = StrokeCap.Round)
+         )
         }
+        Text(
+            text =(curPercentage.value * number).toInt().toString(),
+            color = Color.Black,
+            fontSize = fontSize,
+            fontWeight = FontWeight.Bold
+        )
     }
 }
